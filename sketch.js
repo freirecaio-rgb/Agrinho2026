2096762c51bb13e1eaac0960af9a59f3f39272e9
freirecaio-rgb <freire.caio@escola.pr.gov.br>
2026-06-30 20:35:32 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,22 @@
+let x = 50;
 
+function setup() {
+  let canvas = createCanvas(windowWidth, 150);
+  canvas.position(0, 0);
+  canvas.style('pointer-events', 'none');
+}
+
+function draw() {
+  clear();
+
+  fill(34,139,34);
+  noStroke();
+
+  ellipse(x,75,25,25);
+
+  x += 2;
+
+  if(x > width){
+    x = 0;
+  }
+}
